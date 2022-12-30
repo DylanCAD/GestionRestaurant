915b71fa72e1047a544b5b01ac54f4671a82f0fa
Dylan <cadotdylan7@gmail.com>
2022-12-31 00:52:10 +0100

    ajout menu dylan

--- src/Controller/Admin/MenuController.php
+++ src/Controller/Admin/MenuController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Menu;
+use App\Form\MenuType;
 use App\Repository\MenuRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +21,25 @@ class MenuController extends AbstractController
         $menus=$repo->findAll();
         return $this->render('admin/menu/listeMenus.html.twig', [
             'lesMenus' => $menus
+        ]);
+    }
+
+    /**
+     * @Route("/admin/menu/ajout", name="admin_menu_ajout", methods={"GET","POST"})
+     */
+    public function ajoutMenu( Request $request, EntityManagerInterface $manager)
+    {
+        $menu=new Menu();
+        $form=$this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid() )
+        {
+            $manager->persist($menu);
+            $manager->flush();
+            return $this->redirectToRoute('admin_menus');
+        }
+        return $this->render('admin/menu/formAjoutMenu.html.twig', [
+            'formMenu' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/MenuType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomMenu', TextType::class,[
+                'label'=> "Nom du menu",
+                'attr'=>[
+                    "placeholder"=>"Saisir le nom du menu"
+                ]
+            ])
+            ->add('prixMenu', NumberType::class,[
+                'label'=> "Prix du menu",
+                'attr'=>[
+                    "placeholder"=>"Saisir le prix du menu"
+                ]
+            ])
+            ->add('imageMenu', TextType::class,[
+                'label'=> "Image du menu",
+                'attr'=>[
+                    "placeholder"=>"Saisir l'image du menu"
+                ]
+            ])
+            ->add('descriptionMenu', TextareaType::class,[
+                'label'=> "Description du menu",
+                'attr'=>[
+                    "placeholder"=>"Saisir la description du menu"
+                ]
+            ])
+            ->add('type', ChoiceType::class, [ 
+                'label'=> "Type du menu",
+                "choices"=>[
+                    "Burger"=>1,
+                    "Hot-Dog"=>2,
+                    "Salade"=>3,
+                    "Grillade"=>4,
+                    "Quesadillas"=>5
+                ]
+            ])
+            //->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
